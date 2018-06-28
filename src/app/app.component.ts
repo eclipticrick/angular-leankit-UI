183071e4f0ba71e4f07584c278c6375c5593fc67
eclipticrick <eclipticrick@gmail.com>
2018-06-28 19:20:13 +0200

    added a lot of random components to test theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
 import { routeAnimation } from './app.animations';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,30 @@ import { routeAnimation } from './app.animations';
   styleUrls: ['./app.component.scss'],
   animations: [ routeAnimation ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  theme = 'theme-hu-light';
+  @HostBinding('class') hostClasses: string = this.theme;
+
+  constructor(private overlayContainer: OverlayContainer) {
+    this.overlayContainer.getContainerElement().classList.add(this.theme);
+  }
+
+  ngOnInit() {
+    this.onThemeChange(this.theme);
+  }
+
   getComponentDepth(outlet) {
     return outlet.activatedRouteData['depth'];
+  }
+
+  onThemeChange(theme) {
+    // set theme class on <app-root>
+    this.hostClasses = theme;
+
+    // set theme class on overlayContainer
+    const overlayContainerClasses = this.overlayContainer.getContainerElement().classList;
+    overlayContainerClasses.remove(this.theme);
+    this.theme = theme;
+    overlayContainerClasses.add(this.theme);
   }
 }
